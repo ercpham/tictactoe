b091d4a489bdc07f123fb0f63fe5ab707294774e
ercpham <erc.pham@gmail.com>
2020-09-18 13:31:58 -0700

    Add buttons, grid, clean up UI, refactor code for use of player objects

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
 const gameBoard = (() => {
+    let gameEnd = false;
     let gameArray = [null, null, null, null, null, null, null, null, null];
     const getArray = () => gameArray;
     const setArray = (array) => gameArray = array;
-    const setPosition = (position, character) => gameArray[position] = character;
-    const checkWin = (position) => {
-        const player = gameArray[parseInt(position)];
-        // Check column
+    const setPosition = (position, player) => player != null ? gameArray[position] = player.getName(): gameArray[position] = null;
+    const getGameEnd = () => gameEnd;
+    const setGameEnd = (state) => gameEnd = state;
+    const checkWin = (position, player) => {
+
+        // Check Column
         for (let i = position % 3; i < gameArray.length; i += 3) {
-            if (player !== gameArray[i])
+            if (player.getName() !== gameArray[i])
                 break;
             if (i + 3 >= gameArray.length){
                 displayController.win(player);
@@ -15,8 +18,9 @@ const gameBoard = (() => {
             }
         }
 
+        // Check Row
         for (let i = position - (position % 3); i < position - (position % 3) + 3; i++) {
-            if (player !== gameArray[i])
+            if (player.getName() !== gameArray[i])
                 break;
             if (i % 3 === 2){
                 displayController.win(player);
@@ -25,9 +29,11 @@ const gameBoard = (() => {
         }
         
         if (position % 2 == 0) {
+
+            // Check top-left to bottom-right diagonal
             for (let i = 0; i < gameArray.length; i+=4) {
                 
-                if (player !== gameArray[i])
+                if (player.getName() !== gameArray[i])
                     break;
                 if (i + 1 == gameArray.length){
                     displayController.win(player);
@@ -35,23 +41,35 @@ const gameBoard = (() => {
                 }
             }
 
+            // Check top-right to bottom-left diagonal
             for (let i = 2; i <= 6; i += 2){
-                if (player !== gameArray[i])
+                if (player.getName() !== gameArray[i])
                     break;
                 if (i == 6){
                     displayController.win(player);
                     return;
                 }
             }
-
         }
+
+        if (!gameArray.includes(null))
+            displayController.win(null);
         
     }
-    return {getArray, setArray, setPosition, checkWin};
+    return {getArray, setArray, setPosition, checkWin, getGameEnd, setGameEnd};
 })();
 
 const displayController = (() => {
-    let player = 'O';
+    let playerX;
+    let playerO;
+    let player;
+
+    const setPlayers = (player1, player2) => {
+        playerX = player1;
+        playerO = player2;
+        player = playerX;
+        setToMove();
+    }
 
     const populateBoard = array => {
         const board = document.querySelector(".board");
@@ -68,29 +86,73 @@ const displayController = (() => {
     }
 
     const clearBoard = () => {
+        gameBoard.setGameEnd(false);
         const entries = document.querySelectorAll(".board div p");
         entries.forEach(entry => entry.textContent = "");
         for (let i = 0; i < gameBoard.getArray().length; i++) {
             gameBoard.setPosition(i, null);
         };
+        document.getElementById("winMessage").textContent = "";
     }
 
-    const toggleCurrentCharacter = () => player === 'O' ? player = 'X' : player = 'O';
+    const toggleCurrentCharacter = () => player.getName() === 'O' ? player = playerX : player = playerO;
 
     const win = (player) => {
-        document.getElementById("winMessage").textContent = `${player} wins!`
+        gameBoard.setGameEnd(true);
+        if (player !== null) {
+            document.getElementById("winMessage").textContent = `${player.getName()} wins!`;
+            player.incrementPoints();
+            updateScoreBoard();
+        }
+        else {
+            document.getElementById("winMessage").textContent = `Draw!`
+        }
+    }
+
+    const resetScores = () => {
+        playerX.resetPoints();
+        playerO.resetPoints();
+        updateScoreBoard();
+    }
+
+    const updateScoreBoard = () => {
+        document.getElementById("pXPoints").textContent = playerX.getPoints();
+        document.getElementById("pOPoints").textContent = playerO.getPoints();
+    }
+
+    function setToMove() {
+        document.getElementById("move").textContent = `${player.getName()} to move.`;
     }
 
     function mark() {
-        this.firstChild.textContent = player;
-        gameBoard.setPosition(this.id, player);
-        gameBoard.checkWin(this.id);
+        if (!gameBoard.getGameEnd() && this.firstChild.textContent == "") {
+            this.firstChild.textContent = player.getName();
+            gameBoard.setPosition(this.id, player);
+            gameBoard.checkWin(this.id, player);
+            displayController.toggleCurrentCharacter();
+            setToMove();
+        }
     }
-    return {populateBoard, clearBoard, toggleCurrentCharacter, win};
+    return {populateBoard, clearBoard, toggleCurrentCharacter, setPlayers, win, updateScoreBoard, resetScores};
 })();
 
-const player = () => {
-    return {};
+const player = (xo) => {
+    const name = xo;
+    let points = 0;
+    const getName = () => name;
+    const getPoints = () => points;
+    const incrementPoints = () => points++;
+    const resetPoints = () => points = 0;
+    return {getName, getPoints, incrementPoints, resetPoints};
 }
 
+let clearButton = document.getElementById("clear");
+clearButton.addEventListener("click", displayController.clearBoard);
+let resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", displayController.resetScores);
+
+let playerX = player("X");
+let playerO = player("O");
+
+displayController.setPlayers(playerX, playerO);
 displayController.populateBoard(gameBoard.getArray());
