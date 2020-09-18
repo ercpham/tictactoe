7f53da381d50816fc6b32605959f6102d110e49d
ercpham <erc.pham@gmail.com>
2020-09-17 23:44:25 -0700

    Add win checking

--- scripts.js
+++ scripts.js
@@ -1,9 +1,53 @@
 const gameBoard = (() => {
-    let gameArray = ['X', null, 'X', 'O', 'X', 'O', null, null, null];
+    let gameArray = [null, null, null, null, null, null, null, null, null];
     const getArray = () => gameArray;
     const setArray = (array) => gameArray = array;
     const setPosition = (position, character) => gameArray[position] = character;
-    return {getArray, setArray, setPosition};
+    const checkWin = (position) => {
+        const player = gameArray[parseInt(position)];
+        // Check column
+        for (let i = position % 3; i < gameArray.length; i += 3) {
+            if (player !== gameArray[i])
+                break;
+            if (i + 3 >= gameArray.length){
+                displayController.win(player);
+                return;
+            }
+        }
+
+        for (let i = position - (position % 3); i < position - (position % 3) + 3; i++) {
+            if (player !== gameArray[i])
+                break;
+            if (i % 3 === 2){
+                displayController.win(player);
+                return;
+            }
+        }
+        
+        if (position % 2 == 0) {
+            for (let i = 0; i < gameArray.length; i+=4) {
+                
+                if (player !== gameArray[i])
+                    break;
+                if (i + 1 == gameArray.length){
+                    displayController.win(player);
+                    return;
+                }
+            }
+
+            for (let i = 2; i <= 6; i += 2){
+                if (player !== gameArray[i])
+                    break;
+                if (i == 6){
+                    displayController.win(player);
+                    return;
+                }
+            }
+
+        }
+        
+    }
+    return {getArray, setArray, setPosition, checkWin};
 })();
 
 const displayController = (() => {
@@ -24,29 +68,29 @@ const displayController = (() => {
     }
 
     const clearBoard = () => {
-        const entries = document.querySelectorAll(".board div");
-        entries.forEach(entry => entry.parentNode.removeChild(entry));
+        const entries = document.querySelectorAll(".board div p");
+        entries.forEach(entry => entry.textContent = "");
+        for (let i = 0; i < gameBoard.getArray().length; i++) {
+            gameBoard.setPosition(i, null);
+        };
     }
 
     const toggleCurrentCharacter = () => player === 'O' ? player = 'X' : player = 'O';
 
+    const win = (player) => {
+        document.getElementById("winMessage").textContent = `${player} wins!`
+    }
+
     function mark() {
         this.firstChild.textContent = player;
         gameBoard.setPosition(this.id, player);
+        gameBoard.checkWin(this.id);
     }
-    return {populateBoard, clearBoard, toggleCurrentCharacter};
+    return {populateBoard, clearBoard, toggleCurrentCharacter, win};
 })();
 
 const player = () => {
     return {};
 }
-
-displayController.populateBoard(gameBoard.getArray());
-
-gameBoard.setArray([null, null, 'X', 'O', 'X', 'O', null, null, null]);
-
-displayController.clearBoard();
-
-gameBoard.setPosition(1, 'X');
 
 displayController.populateBoard(gameBoard.getArray());
